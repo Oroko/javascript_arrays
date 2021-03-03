d3e71cfe05a9e860dba7f0601597aa916a84bbcc
Daniel Carbajal <carbajal869@gmail.com>
2021-03-03 14:20:45 -0500

    Copying an array using Slice method

---
+++ copyarrayquiz.js
@@ -0,0 +1,11 @@
+// This quiz includes topics from this lesson: "Slice". If you find this quiz difficult, revisiting that lesson may help!
+// Write a function copyArray that returns a copy of an array. Changes to the copy shouldn't affect the original array and vice-versa.
+
+
+
+function copyArray(arr){
+  return  arr.slice()
+}
+
+
+console.log(copyArray([1, 2, 3, 5]))
